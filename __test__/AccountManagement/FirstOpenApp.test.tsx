9b9580910e8dc31170bb8ce9e39a17815613e57d
Shiraishi-Shodai <siranosuke1227@gmail.com>
2024-11-11 13:19:11 +0900

    lib/PersonDAO.tsxを削除

--- __test__/AccountManagement/FirstOpenApp.test.tsx
+++ __test__/AccountManagement/FirstOpenApp.test.tsx
@@ -4,7 +4,7 @@
 import React, { useState } from "react";
 import { render } from "@testing-library/react-native";
 import { Redirect } from "expo-router";
-import Layout from "@/app/(tabs)/_layout"; // テストするコンポーネントのパスに合わせて変更
+import TabLayout from "@/app/(tabs)/_layout"; // テストするコンポーネントのパスに合わせて変更
 import { AuthContext } from "@/components/AuthProvider";
 import { User } from "@/classies/User";
 
@@ -21,7 +21,7 @@ const TestProvider = () => {
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      <Layout />
+      <TabLayout />
     </AuthContext.Provider>
   );
 };
@@ -30,7 +30,7 @@ describe("Users who have never logged in before launch the application.", () => 
   test("Should redirect to login screen", () => {
     render(<TestProvider />);
 
-    //   expect(Redirect).toHaveBeenCalled();
+    expect(Redirect).toHaveBeenCalled();
     expect(Redirect).toHaveBeenCalledTimes(1);
     expect(Redirect.mock.calls[0][0]).toStrictEqual({ href: "/login" });
   });
